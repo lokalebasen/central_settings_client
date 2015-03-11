ae86855851306c00ac9cdae3c9682b93ace8fdb6
Anders Emil Nielsen <aemilnielsen@gmail.com>
2015-03-11 13:53:23 +0100

    Support cache settings by multiple cache keys

--- lib/lokalebasen_settings_client/caching_client.rb
+++ lib/lokalebasen_settings_client/caching_client.rb
@@ -17,14 +17,20 @@ module LokalebasenSettingsClient
     end
 
     def by_site_key(site_key)
-      cache.cached { client.json_settings_by_site_key(site_key) }
+      cache.cached site_key_cache_key(site_key) do
+        client.json_settings_by_site_key(site_key)
+      end
     rescue Exception => e
       Airbrake.notify(e) if defined?(Airbrake)
       raise e if @reraise_error
-      cache.last_cached_value
+      cache.last_cached_value site_key_cache_key(site_key)
     end
 
     private
+
+    def site_key_cache_key(site_key)
+      "site_key_#{site_key}"
+    end
 
     def cache
       @cache ||= SettingsCache.new
--- spec/lokalebasen_settings_client/settings_cache_spec.rb
+++ spec/lokalebasen_settings_client/settings_cache_spec.rb
@@ -4,41 +4,42 @@ require_relative '../../lib/lokalebasen_settings_client/settings_cache'
 describe LokalebasenSettingsClient::SettingsCache do
   let(:cache_time) { 60 * 60 }
   let(:cache) { LokalebasenSettingsClient::SettingsCache.new(cache_time) }
+  let(:cache_key) { 'site_key_dk' }
 
   it "responds with the newly cached value" do
-    expect(cache.cached { 'hello world' }).to eq('hello world')
+    expect(cache.cached(cache_key) { 'hello world' }).to eq('hello world')
   end
 
   it "responds with the earlier cached value when within caching time" do
-    cache.cached { 'my cool world' }
-    expect(cache.cached { 'my new cool world' }).to eq('my cool world')
+    cache.cached(cache_key) { 'my cool world' }
+    expect(cache.cached(cache_key) { 'my new cool world' }).to eq('my cool world')
   end
 
   it "caches new value when cache is busted" do
-    cache.cached { 'hello hello' }
+    cache.cached(cache_key) { 'hello hello' }
     Timecop.travel(Time.now + cache_time + 1) do
-      expect(cache.cached { 'new value' }).to eq('new value')
+      expect(cache.cached(cache_key) { 'new value' }).to eq('new value')
     end
   end
 
   it "postpones cache when exception is raised" do
-    cache.cached { 'hello my world' }
+    cache.cached(cache_key) { 'hello my world' }
     Timecop.travel(Time.now + cache_time + 1) do
-      expect { cache.cached { fail StandardError } }.to raise_error
-      expect(cache.cached { 'new hello world'}).to eq('hello my world')
+      expect { cache.cached(cache_key) { fail StandardError } }.to raise_error
+      expect(cache.cached(cache_key) { 'new hello world'}).to eq('hello my world')
     end
   end
 
   it "returns last cached value" do
-    cache.cached { 'hello world' }
-    expect(cache.last_cached_value).to eq('hello world')
+    cache.cached(cache_key) { 'hello world' }
+    expect(cache.last_cached_value(cache_key)).to eq('hello world')
   end
 
   it "is able to returned the last cached value after failed caching" do
-    cache.cached { 'hello my cool world' }
+    cache.cached(cache_key) { 'hello my cool world' }
     Timecop.travel(Time.now + cache_time + 1) do
-      expect { cache.cached { fail StandardError } }.to raise_error
-      expect(cache.last_cached_value).to eq('hello my cool world')
+      expect { cache.cached(cache_key) { fail StandardError } }.to raise_error
+      expect(cache.last_cached_value(cache_key)).to eq('hello my cool world')
     end
   end
 
@@ -46,10 +47,15 @@ describe LokalebasenSettingsClient::SettingsCache do
   # we don't want to spend 0.5 seconds on fetching settings. Instead
   # we just wait for another hour before trying to fetch the latest settings
   it "postpones cache expiration when cache attempt failes" do
-    cache.cached { 'hello world' }
+    cache.cached(cache_key) { 'hello world' }
     Timecop.travel(Time.now + cache_time + 1) do
-      expect { cache.cached { fail StandardError } }.to raise_error
-      expect(cache.cached { 'new value' }).to eq('hello world')
+      expect { cache.cached(cache_key) { fail StandardError } }.to raise_error
+      expect(cache.cached(cache_key) { 'new value' }).to eq('hello world')
     end
+  end
+
+  it "does not reuse the cache value for another key" do
+    cache.cached('site_key_dk') { 'hello dk!' }
+    expect(cache.cached('site_key_se') { 'hello se!' }).to eq('hello se!')
   end
 end
